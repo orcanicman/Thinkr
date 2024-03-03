a67e2ec73ef0084ae724ee7d8fdd259c7192b89c
bjorn <orcanicman@gmail.com>
2024-03-03 22:37:20 +0100

    Initial styling

--- Thinkr-app/frontend/tailwind.config.ts
+++ Thinkr-app/frontend/tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      ownWhite: "#FFFFFF",
+      ownCream: "#E5E5E5",
+      ownBlack: "#2D2D2D",
+      ownBlue: "#093545",
+      ownDarkBlue: "#262C30",
+      ownLightBlue: "#224957",
+      ownGreen: "#20DF7F",
     },
   },
   plugins: [],
