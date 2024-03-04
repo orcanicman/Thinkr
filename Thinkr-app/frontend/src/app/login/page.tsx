ee036026a2c19bb4c5536cff6c49aac081b75fd7
Bjorn <bjornpostemaster@gmail.com>
2024-03-04 21:33:57 +0100

    fixed styling

--- Thinkr-app/frontend/src/app/login/page.tsx
+++ Thinkr-app/frontend/src/app/login/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col sm:items-center sm:p-0 p-6">
-      <header className="flex flex-col items-center">
+      <header className="flex flex-col items-center mb-4">
         <Image
           alt="Logo"
           src={"Thinkr-logo.svg"}
@@ -16,8 +16,8 @@ export default function Home() {
         <h6 className="font-light">&ldquo;Speak your mind&rdquo;</h6>
       </header>
 
-      <form className="flex flex-col sm:max-w-96">
-        <h1 className="font-bold sm:text-7xl text-4xl sm:mx-16 sm:my-12 my-6">
+      <form className="flex flex-col sm:w-96 sm:items-center">
+        <h1 className="font-bold sm:text-7xl text-6xl sm:my-12 my-6">
           Sign in
         </h1>
         <h6 className="hidden sm:block font-light mb-8">
@@ -26,16 +26,16 @@ export default function Home() {
 
         <input
           type="email"
-          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-3 mb-6 w-full"
+          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-4 mb-4 w-full"
           placeholder="Email"
         />
         <input
           type="password"
-          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-3 mb-6 w-full"
+          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-4 mb-4 w-full"
           placeholder="Password"
         />
 
-        <label className="w-full flex items-center space-x-2 mb-6">
+        <label className="w-full flex items-center space-x-2 mb-4">
           <input
             type="checkbox"
             className="appearance-none bg-ownLightBlue checked:bg-ownGreen p-2 rounded cursor-pointer"
@@ -43,7 +43,10 @@ export default function Home() {
           <span>Remember me</span>
         </label>
 
-        <button type="submit" className="w-full bg-ownGreen rounded-lg py-3">
+        <button
+          type="submit"
+          className="w-full bg-ownGreen rounded-lg py-3 shadow-ownBlack shadow-lg hover:opacity-80"
+        >
           Login
         </button>
       </form>
