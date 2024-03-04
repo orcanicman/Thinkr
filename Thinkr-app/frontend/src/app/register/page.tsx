5f36f1f1bf1e3a1c5b5f0bb4c7e25a279683536f
Bjorn <bjornpostemaster@gmail.com>
2024-03-04 21:33:52 +0100

    Added register page

---
+++ Thinkr-app/frontend/src/app/register/page.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col sm:items-center sm:p-0 p-6">
+      <header className="flex flex-col items-center mb-4">
+        <Image
+          alt="Logo"
+          src={"Thinkr-logo.svg"}
+          width={58}
+          height={58}
+          className="m-6"
+        />
+
+        <h2 className="font-bold text-4xl">Thinkr</h2>
+        <h6 className="font-light">&ldquo;Speak your mind&rdquo;</h6>
+      </header>
+
+      <form className="flex flex-col sm:w-96 sm:items-center">
+        <h1 className="font-bold sm:text-7xl text-4xl sm:my-12 my-6">
+          Register
+        </h1>
+        <h6 className="hidden sm:block font-light mb-8">
+          Make an account to see what your friends are thinking!
+        </h6>
+
+        <input
+          type="text"
+          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-4 mb-4 w-full"
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-4 mb-4 w-full"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className="rounded-lg bg-ownLightBlue placeholder:text-ownWhite px-5 py-4 mb-4 w-full"
+          placeholder="Password"
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-ownGreen rounded-lg py-3 shadow-ownBlack shadow-lg hover:opacity-80"
+        >
+          Register
+        </button>
+
+        <Link
+          href={"login"}
+          className="text-ownGreen underline my-4 self-start"
+        >
+          Already have an account?
+        </Link>
+      </form>
+
+      <footer className="overflow-hidden absolute bottom-0 w-screen sm:h-24 lg:h-60 -z-10">
+        <Image
+          alt="LOL2"
+          src={"Thinkr-login-blueFooter.svg"}
+          width={1280}
+          height={111}
+          className="absolute left-0 -bottom-10 w-screen opacity-80 dark:opacity-20"
+        />
+        <Image
+          alt="LOL"
+          src={"Thinkr-login-greenFooter.svg"}
+          width={1280}
+          height={111}
+          className="absolute left-0 -bottom-10 w-screen opacity-80 dark:opacity-20"
+        />
+      </footer>
+    </main>
+  );
+}
