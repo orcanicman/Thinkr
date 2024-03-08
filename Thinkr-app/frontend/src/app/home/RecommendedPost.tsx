dd8fcf3d0dc72c4377455e27a1acfbaa1c487666
Bjorn <bjornpostemaster@gmail.com>
2024-03-08 15:02:03 +0100

    split recommended post up

---
+++ Thinkr-app/frontend/src/app/home/RecommendedPost.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export const RecommendedPost = ({
+  likes,
+  link,
+  title,
+}: {
+  title: string;
+  likes: number;
+  link: string;
+}) => {
+  return (
+    <Link href={link} className="mb-4 flex w-full justify-between space-x-2">
+      <div className="">
+        <h2 className="mb-1 max-w-60 overflow-hidden overflow-ellipsis whitespace-nowrap hover:underline">
+          {title}
+        </h2>
+        <h3 className="text-ownGrey">
+          {Intl.NumberFormat("en-US", {
+            notation: "compact",
+            maximumFractionDigits: 1,
+          }).format(likes)}{" "}
+          Likes
+        </h3>
+      </div>
+      <button className="min-w-7">
+        <Image
+          alt="Settings"
+          src={"menu-meatball.svg"}
+          width={26}
+          height={26}
+        />
+      </button>
+    </Link>
+  );
+};
--- Thinkr-app/frontend/src/app/home/page.tsx
+++ Thinkr-app/frontend/src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { InputBox } from "./InputBox";
+import { RecommendedPost } from "./RecommendedPost";
 
 export default function Home() {
   return (
@@ -145,43 +146,10 @@ const Recommended = ({ className }: { className?: string }) => {
         likes={255}
         link="#"
       />
-      {/* TODO: Formatting for the likes: {Intl.NumberFormat("en-US", {
-            notation: "compact",
-            maximumFractionDigits: 1,
-          }).format(likes)} */}
       <RecommendedPost title="PLACEHOLDER TITLE 2" likes={1252} link="#" />
       <Link href={"#"} className="text-ownGreen hover:underline">
         Show more
       </Link>
     </section>
-  );
-};
-
-const RecommendedPost = ({
-  likes,
-  link,
-  title,
-}: {
-  title: string;
-  likes: number;
-  link: string;
-}) => {
-  return (
-    <Link href={link} className="mb-4 flex w-full justify-between space-x-2">
-      <div className="">
-        <h2 className="mb-1 max-w-60 overflow-hidden overflow-ellipsis whitespace-nowrap hover:underline">
-          {title}
-        </h2>
-        <h3 className="text-ownGrey">{likes} Likes</h3>
-      </div>
-      <button className="min-w-7">
-        <Image
-          alt="Settings"
-          src={"menu-meatball.svg"}
-          width={26}
-          height={26}
-        />
-      </button>
-    </Link>
   );
 };
